78ee80846ac32e01c71512147b734e5a81dea94d
PhuocPhung <phuocphung5890@gmail.com>
2025-12-14 00:51:00 +0700

    sửa smee v2

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-// import SmeeClient from 'smee-client';
 import cors from "cors";
 import morgan from "morgan";
-// import { setGlobalDispatcher, Agent } from 'undici';
 
 // Load environment variables
 dotenv.config();
@@ -15,14 +13,6 @@ import authRoutes from "./routes/auth.js";
 import accountRoutes from "./routes/accounts.js";
 import departmentRoutes from "./routes/departments.js";
 import requestRoutes from "./routes/requests.js";
-
-// Undici global agent configuration
-// setGlobalDispatcher(
-//   new Agent({
-//     headersTimeout: 0, // TẮT timeout headers
-//     bodyTimeout: 0,
-//   })
-// );
 
 // Services
 import { initGmailWatcher } from "./services/email_ggapi.js";
@@ -74,7 +64,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-
 // Default route
 app.get("/", (req, res) => {
   res.json({ message: "Ecommerce API is running!" });
@@ -83,30 +72,10 @@ app.get("/", (req, res) => {
 // Connect to database and start server
 const PORT = process.env.PORT;
 
-// Connect to watch Gmail
-// const watch_Gmail = async () => {
-//   // Smee Client setup để chuyển tiếp url từ smee.io về máy cục bộ
-//     const smee = new SmeeClient({
-//       source: process.env.SMEE_CLIENT_URL, // URL của kênh Smee.io
-//       target: process.env.SMEE_TARGET_URL, // URL đích trên máy cục bộ của bạn
-//       logger: console
-//     });
-
-//     smee.start();
-//     // Chuyển địa chỉ để subscription Pub/Sub của Gmail dùng được http://localhost:5000/api/requests/pubsub
-//     // console.log(`Smee client started, forwarding events from ${process.env.SMEE_CLIENT_URL} to ${process.env.SMEE_TARGET_URL}`);
-
-//     // Khởi động Gmail Watcher
-//     await initGmailWatcher();
-// }
-// khởi động mô hình AI
-
 connectDB().then(() => {
   app.listen(PORT, async () => {
     console.log(`Server running on port ${PORT}`);
     
-    // Connect to watch Gmail
-    // await watch_Gmail();
     // Khởi động Gmail Watcher
     await initGmailWatcher();
 
